e053809cb948b95fc1de01cc88c59ec19cd52d50
tobi freund <tobias.j.freund@gmail.com>
2023-10-30 11:26:20 +0100

    Aufgabe 2

---
+++ summieren.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream f_zahlen("daten.txt");
+    ofstream f_summe("datensumme.txt");
+    int zahl1, zahl2;
+    f_zahlen >> zahl1;
+    f_zahlen >> zahl2;
+    
+    while(!f_zahlen.eof()) {
+        // cout << zahl1 << " + " << zahl2 << " = " << zahl1+zahl2 << endl;
+        f_summe << zahl1+zahl2 << endl;
+        f_zahlen >> zahl1;
+        f_zahlen >> zahl2;
+    }
+
+    return 0;
+}
